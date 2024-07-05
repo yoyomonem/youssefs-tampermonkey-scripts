9ae8fac8c6726a121dbc0f3cf6d598e3810b67c6
Y Nasr <yoyo.monem22@gmail.com>
2024-07-06 00:59:52 +0200

    Scratch 2.0 Maintenance Pages → 3.0 Maintenance Pages (Discussion Forums Only): v1.0.1

--- scratch-2.0-to-scratch-3.0-maintenance/discussion-forums/script.user.js
+++ scratch-2.0-to-scratch-3.0-maintenance/discussion-forums/script.user.js
@@ -2,7 +2,7 @@
 // @name         Scratch 2.0 Maintenance Pages → 3.0 Maintenance Pages (Discussion Forums Only)
 // @namespace    https://scratch.mit.edu
 // @namespace    https://github.com/yoyomonem/youssefs-tampermonkey-scripts
-// @version      1.0
+// @version      1.0.1
 // @description  This turns Scratch 2.0 maintenance pages into maintenance pages that are in the style of Scratch 3.0, like how the Scratch 2.0 → 3.0 addon from Scratch Addons does it with instances of https://scratch.mit.edu/500. This only works with the Scratch Discussion Forums, so I recommend you use this userscript with the Scratch 2.0 → 3.0 addon from Scratch Addons.
 // @author       Youssef
 // @match        https://scratch.mit.edu/discuss/*
@@ -13,7 +13,7 @@
 (function() {
   "use strict";
 
-  try {
+  if (document.querySelector(".box#page-maint")) {
     const style = document.createElement("style");
     style.id = "scratch-2.0-maintenance-pages-to-3.0-maintenance-pages-discussion-forums-only";
     style.setAttribute("data-is-tampermonkey-added-style", true);
@@ -36,7 +36,7 @@
     header.style = "color: rgb(255, 255, 255)";
     const logo = document.querySelector("img[src=\"http://scratch-maintenance.s3.amazonaws.com/scratch.png\"]");
     logo.src = "/images/logo_sm.png";
-  } catch {
+  } else {
     GM_log("The Scratch Discussion Forums are not in maintenance.");
   }
 })();
