96e18b3fe49bb9edbb830d597ec2853bf72fb2bb
Y Nasr <youssef.land@outlook.com>
2025-04-02 19:38:56 +0200

    Scratch 2.0 to 3.0 Maintenance (Misc.) 1.0.1

--- scratch-2.0-to-scratch-3.0-maintenance/misc/script.user.js
+++ scratch-2.0-to-scratch-3.0-maintenance/misc/script.user.js
@@ -2,7 +2,7 @@
 // @name         Scratch 2.0 Maintenance Pages → 3.0 Maintenance Pages (Miscanellous)
 // @namespace    https://scratch.mit.edu
 // @namespace    https://github.com/yoyomonem/youssefs-tampermonkey-scripts
-// @version      1.0.0
+// @version      1.0.1
 // @description  This turns Scratch 2.0 maintenance pages into maintenance pages that are in the style of Scratch 3.0, in a similar manner to how the Scratch 2.0 → 3.0 addon from Scratch Addons does it with some instances of https://scratch.mit.edu/500. This works with any page on Scratch except for the Discussion Forums, so I recommend you use this userscript with the Scratch 2.0 → 3.0 addon from Scratch Addons.
 // @author       Youssef
 // @match        https://scratch.mit.edu/*
@@ -13,7 +13,6 @@
 
 (function() {
   "use strict";
-
   if (document.querySelector(".box#page-500")) {
     const style = document.createElement("style");
     style.id = "scratch-2.0-maintenance-pages-to-3.0-maintenance-pages-discussion-forums-only";
@@ -35,7 +34,11 @@
     document.body.appendChild(style);
     const header = document.querySelector(".box-header h1");
     header.style = "color: rgb(255, 255, 255)";
+    const imgs = document.querySelectorAll("img");
+    imgs[1].src = "/images/logo_sm.png";
+    GM_log("This page is templates/500.html");
   } else {
     GM_log("Scratch's servers aren't having issues.");
+    GM_log("This page isn't templates/500.html");
   }
 })();
